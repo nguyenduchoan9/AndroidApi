27c2a898c004dd6b4adc128753dd03b132c9d424
Nam Nhat <Nam Nhat>
2016-12-04 11:20:43 +0700

    aa

---
+++ AnroidApi/Models/OrderDao.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnroidApi.Models
+{
+    public class OrderDao
+    {
+        BookManagementEntities db;
+        public OrderDao()
+        {
+            db = new BookManagementEntities();
+        }
+        public bool createOrder(Order order)
+        {
+            try
+            {
+                var entity = new Order
+                {
+                    ID = order.ID,
+                    UserName = order.UserName,
+                    UserID = order.UserID,
+                    StartDate = order.StartDate,
+                    EndDate = order.EndDate,
+                    StatusID = order.StatusID,
+                    Note = order.Note,
+                    Address = order.Address,
+                    CreatedDate = order.CreatedDate,
+                };
+                db.Orders.Add(entity);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ AnroidApi/Models/OrderViewModels.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnroidApi.Models
+{
+    public class OrderViewModels
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public int UserID { get; set; }
+        public System.DateTime StartDate { get; set; }
+        public System.DateTime EndDate { get; set; }
+        public int StatusID { get; set; }
+        public string Note { get; set; }
+        public string Address { get; set; }
+       public System.DateTime CreatedDate { get; set; }
+        public OrderViewModels()
+        {
+
+        }
+        public OrderViewModels(Order order)
+        {
+            ID = order.ID;
+            UserName = order.UserName;
+            UserID = order.UserID;
+            StartDate = order.StartDate;
+            EndDate = order.EndDate;
+            StatusID = order.StatusID;
+            Note = order.Note;
+            Address = order.Address;
+            CreatedDate = order.CreatedDate;
+        }
+    }
+}
